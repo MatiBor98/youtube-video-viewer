aea674d40ee93bc5a1d1abe4dfee86a1fbdd7f06
Matias Borggio <mati98bor@hotmail.com>
2021-11-27 19:13:03 -0300

    search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Button,
+  Form,
+  Col,
+  Row,
+  Container,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaSearch } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{ backgroundColor: "green" }}>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Recipient's username"
+          ></input>
+          <div class="input-group-append">
+            <button class="btn btn-secondary" type="button">
+              Button
+            </button>
+          </div>
+        </div>
+        <Row>
+          <Col sm="8" className="my-5" style={{ backgroundColor: "green" }}>
+            <Form className="d-flex">
+              <InputGroup>
+                <InputGroup.Text style={{ backgroundColor: "white" }}>
+                  <FaSearch />
+                </InputGroup.Text>
+                <FormControl
+                  id="inlineFormInputGroup"
+                  placeholder="Search..."
+                />
+                <div className="input-group-prepend">
+                  <Button type="submit" variant="dark">
+                    Search
+                  </Button>
+                </div>
+              </InputGroup>
+            </Form>
+          </Col>
+          <Col sm="4" style={{ display: "flex", justifyContent: "right" }}>
+            2 of 2
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
